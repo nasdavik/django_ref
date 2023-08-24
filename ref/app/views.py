1a768dd09fd7483bd20df82b422e8aa86bea69fd
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-08-24 13:12:12 +0500

    The application logic has been edited

--- ref/app/views.py
+++ ref/app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
-from .serializers import UserSerializer
 import random
 import time
 
@@ -15,10 +14,10 @@ def send_auth_code(request):
     if not user:
         # Если пользователь не найден, создаем новую запись
         user = User.objects.create(phone_number=phone_number)
-    else:
-        # Если пользователь существует, генерируем новый код авторизации
-        user.auth_code = random.randint(1000, 9999)
-        user.save()
+
+    # Если пользователь существует, генерируем новый код авторизации
+    user.auth_code = random.randint(1000, 9999)
+    user.save()
 
     # Имитация задержки на сервере
     time.sleep(2)
@@ -32,11 +31,15 @@ def verify_auth_code(request):
     auth_code = request.data['auth_code']
     user = get_object_or_404(User, phone_number=phone_number, auth_code=auth_code)
 
+    # При первой авторизации генерируем и присваиваем инвайт-код
+    if not user.invite_code:
+        user.invite_code = ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(6))
+
     # Сбрасываем код авторизации после успешной проверки
     user.auth_code = None
     user.save()
 
-    return Response({'message': 'Авторизация выполнена успешно'})
+    return Response({'message': 'Авторизация выполнена успешно', 'invate_code': f'{user.invite_code}'})
 
 
 @api_view(['POST'])
@@ -45,20 +48,17 @@ def update_invite_code(request):
     invite_code = request.data['invite_code']
     user = get_object_or_404(User, phone_number=phone_number)
 
-    # При первой авторизации генерируем и присваиваем инвайт-код
-    if not user.invite_code:
-        user.invite_code = ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(6))
-    else:
-        # Если пользователь уже активировал инвайт-код, выводим его
-        user.activated_invite_code = user.invite_code
+    if user.activated_invite_code:
+        return Response({'message': f'Ваш инвайт-код: {user.activated_invite_code}'})
 
     # Проверяем существование чужого инвайт-кода и обновляем его
-    if User.objects.filter(invite_code=invite_code).exists():
-        user.invite_code = invite_code
+    elif User.objects.filter(invite_code=invite_code).exists():
+        user.activated_invite_code = user.invite_code
+        user.save()
+        return Response({'message': 'Инвайт-код обновлен'})
 
-    user.save()
-
-    return Response({'message': 'Инвайт-код обновлен'})
+    else:
+        return Response({'message': 'Такого инвайт-кода не существует. Проверьте, правильно ли вы его написали '})
 
 
 @api_view(['GET'])
